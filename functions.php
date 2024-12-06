2a9e29bf35b731a04a448222feafa9462bb167f3
Dalvae <diego.alvarez.e@ug.uchile.cl>
2024-12-06 16:31:18 -0300

    dev flag

--- functions.php
+++ functions.php
@@ -4,7 +4,10 @@ error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED);
 // Main switch to get frontend assets from a Vite dev server OR from production built folder
 // If you specify a DEVELOPMENT_IP constant, the frontend assets will be loaded from the Vite dev server only for the IP specified
 // it is recommended to move it into wp-config.php
-const IS_VITE_DEVELOPMENT = true;
+
+if (!defined('IS_VITE_DEVELOPMENT')) {
+    define('IS_VITE_DEVELOPMENT', true);
+}
 //const DEVELOPMENT_IP = '123.123.123.123';
 
 require 'vendor/autoload.php';
