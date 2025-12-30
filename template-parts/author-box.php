996552ec002e5a46cd3e76fabf19cd1425bb2cbd
Dalvae <diego.alvarez.e@ug.uchile.cl>
2025-12-30 11:50:21 -0300

    Remove Alegreya SC fonts (~950KB savings)

--- template-parts/author-box.php
+++ template-parts/author-box.php
@@ -4,7 +4,7 @@ if (!$coauthor) return;
 ?>
 <div class="bg-swhite my-7">
     <div class="container lg:w-4/5 mx-auto px-4 py-10 lg:py-10 text-center">
-        <h2 class="text-rojo font-semibold font-als text-3xl mb-5">
+        <h2 class="text-rojo font-semibold text-3xl mb-5">
             <a href="<?php echo esc_url(get_author_posts_url($coauthor->ID)); ?>">
                 <?php echo esc_html($coauthor->display_name); ?>
             </a>
